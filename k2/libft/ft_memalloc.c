16c36485e12728a2fa96960b38028cf0985edce5
Kevin Cosmon <kevin.cosmon@hetic.net>
2019-04-02 16:06:47 +0200

    e

---
+++ k2/libft/ft_memalloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kecosmon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/22 15:26:19 by kecosmon          #+#    #+#             */
+/*   Updated: 2019/02/22 15:26:21 by kecosmon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void *zonemem;
+
+	if (!(zonemem = (char *)malloc(size * sizeof(zonemem))))
+			return (NULL);
+	ft_bzero(zonemem, size);
+	return (zonemem);
+}
